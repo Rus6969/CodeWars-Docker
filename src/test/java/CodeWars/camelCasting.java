38e1c960a6b6974de12bcb4fd38b8161cefc3b14
Rus6969 <russam4515@gmail.com>
2020-04-20 18:51:53 -0400

    didgits Sum

--- src/test/java/CodeWars/camelCasting.java
+++ src/test/java/CodeWars/camelCasting.java
@@ -1,4 +1,28 @@
 package CodeWars;
 
+import java.util.Arrays;
+
+
 public class camelCasting {
+    public static void main(String[] args) {
+        String str = "camelCasting";
+        StringBuilder result = new StringBuilder();
+
+        char[] arr=str.toCharArray();
+        System.out.println(Arrays.toString(arr));
+        for(Character each: arr){
+            if(Character.isUpperCase(each)){
+                result.append(" ").append(each).toString();
+            }else{
+                result.append(each).toString();
+            }
+        }
+        result.toString();
+
+        System.out.println(result);
+
+
+
+
+    }
 }
--- src/test/java/CodeWars/isArraySorted.java
+++ src/test/java/CodeWars/isArraySorted.java
@@ -1,4 +1,15 @@
 package CodeWars;
 
+
+import java.util.Arrays;
+
 public class isArraySorted {
-}
+    public static void main(String[] args) {
+        int arr[]={2,1,3,4,5};
+        int[] sort= new int [arr.length];
+        System.arraycopy(arr,0,sort,0,arr.length);
+        Arrays.sort(sort);
+        System.out.println(Arrays.equals(arr, sort));
+    }
+    }
+
