766b00f647a05974956859f20744b6980d8ab024
Rus6969 <russam4515@gmail.com>
2020-05-30 13:04:09 -0400

    new tasks added

--- src/test/java/CodeWars/Numberofdays.java
+++ src/test/java/CodeWars/Numberofdays.java
@@ -6,15 +6,15 @@ class DaysBetweenDate {
     public static void main(String[] args) {
         // Month value in Java is 0-based so 11 means December.
         Calendar start = Calendar.getInstance();
-        start.set(1992, 10, 4);
+        start.set(1991, 3, 31);
         Calendar end = Calendar.getInstance();
-        end.set(2020, 5, 29);
+        end.set(2020, 5, 30);
 
-        System.out.print(" Number Monday between " +
+        System.out.print(" TIMA Number Fridays between " +
                 start.getTime() + " and " + end.getTime() + " are: ");
 
         int numberOfDays = 0;
-        int monday = Calendar.MONDAY;
+        int monday = Calendar.FRIDAY;
 
         while (start.before(end)) {
             if (start.get(Calendar.DAY_OF_WEEK) == monday) {
