7f043110050bc42f476cbf230872f72b091311e5
Rus6969 <russam4515@gmail.com>
2020-06-05 21:44:48 -0400

    encrypt example

---
+++ src/test/java/CodeWars/EncryptExample.java
@@ -0,0 +1,42 @@
+package CodeWars;
+
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.UnsupportedEncodingException;
+import java.security.*;
+
+public class EncryptExample {
+// link https://www.tutorialspoint.com/java_cryptography/index.htm
+    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, UnsupportedEncodingException {
+        //Creating a Signature object
+        Signature sign = Signature.getInstance("SHA256withRSA");
+
+        //Creating KeyPair generator object
+        KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
+
+        //Initializing the key pair generator
+        keyPairGen.initialize(2048);
+
+        //Generating the pair of keys
+        KeyPair pair = keyPairGen.generateKeyPair();
+
+        //Creating a Cipher object
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+
+        //Initializing a Cipher object
+        cipher.init(Cipher.ENCRYPT_MODE, pair.getPublic());
+
+        //Adding data to the cipher
+        byte[] input = "say yes to life".getBytes();
+        cipher.update(input);
+
+        //encrypting the data
+        byte[] cipherText = cipher.doFinal();
+        System.out.println(new String(cipherText, "UTF8"));
+
+        cipher.init(Cipher.DECRYPT_MODE,pair.getPublic());
+    }
+}
