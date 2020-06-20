105c86177237f3e0a431e70ff0703ea0e581d693
Rus6969 <russam4515@gmail.com>
2020-06-20 12:02:01 -0400

    ggg

---
+++ src/test/java/Docker/ggg.java
@@ -0,0 +1,12 @@
+package Docker;
+
+public class ggg {
+    public static int math(int x , int y){
+        return (x+y);
+    }
+    public static void main(String[] args) {
+        int a =9;
+        int b=1;
+        System.out.println(math(a,b));
+    }
+}
