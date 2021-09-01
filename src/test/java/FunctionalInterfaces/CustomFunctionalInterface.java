2de45540312c59707f0488695facf00d493c879f
RussellRuslan <SamatovR@aetna.com>
2021-09-01 19:40:56 -0400

    labda generic type

--- src/test/java/FunctionalInterfaces/CustomFunctionalInterface.java
+++ src/test/java/FunctionalInterfaces/CustomFunctionalInterface.java
@@ -15,9 +15,17 @@ public class CustomFunctionalInterface {
         System.out.println(twoStrings.function("Java","Phyton"));
         System.out.println("*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
 
-// no need body {} because only one action is taken only one parameter
+// no need body {} because only one action(statement)  is taken only one parameter
         GenericDataFunction<String> reverse= s -> new StringBuilder(s).reverse().toString();
+        // need body example
+        GenericDataFunction<String>reverse2=(s) ->{
+            String result="";
+            for (int i=s.length()-1;i>=0;i--)
+                result+=s.charAt(i);
+            return result;
+        } ;
 
         System.out.println(reverse.function("Ruslan"));
+        System.out.println(reverse2.function("rocket"));
     }
 }
