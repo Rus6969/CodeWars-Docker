588e382f327c887fa4f2b277f557427b92b0a79c
RussellRuslan <SamatovR@aetna.com>
2021-09-01 19:45:17 -0400

    labda generic type

--- src/test/java/FunctionalInterfaces/CustomFunctionalInterface.java
+++ src/test/java/FunctionalInterfaces/CustomFunctionalInterface.java
@@ -6,26 +6,31 @@ public class CustomFunctionalInterface {
         //finding longest strin
 
         TwoStrings twoStrings = (str1, str2) -> {
-            if (str1.length()>str2.length())
+            if (str1.length() > str2.length())
                 return str1;
-                return str2;
-            };
+            return str2;
+        };
 
 
-        System.out.println(twoStrings.function("Java","Phyton"));
+        System.out.println(twoStrings.function("Java", "Phyton"));
         System.out.println("*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
 
 // no need body {} because only one action(statement)  is taken only one parameter
-        GenericDataFunction<String> reverse= s -> new StringBuilder(s).reverse().toString();
+        GenericDataFunction<String> reverse = s -> new StringBuilder(s).reverse().toString();
         // need body example
-        GenericDataFunction<String>reverse2=(s) ->{
-            String result="";
-            for (int i=s.length()-1;i>=0;i--)
-                result+=s.charAt(i);
+        GenericDataFunction<String> reverse2 = (s) -> {
+            String result = "";
+            for (int i = s.length() - 1; i >= 0; i--)
+                result += s.charAt(i);
             return result;
-        } ;
+        };
 
         System.out.println(reverse.function("Ruslan"));
+        System.out.println("-*-*-*-*-*-*-*-* fuction2**-*-*-*-*-*-");
         System.out.println(reverse2.function("rocket"));
+
+        GenericDataFunction<Integer>cube =n -> n*n*n;
+
+        System.out.println(cube.function(3));
     }
 }
