f403e0eabbeebad7093753e3410d1d3f28d79630
RussellRuslan <SamatovR@aetna.com>
2021-09-01 21:52:22 -0400

    Functional

--- src/test/java/FunctionalInterfaces/FunctionFunctionalInterface.java
+++ src/test/java/FunctionalInterfaces/FunctionFunctionalInterface.java
@@ -1,5 +1,11 @@
 package FunctionalInterfaces;
 
+import java.util.function.Function;
+
 public interface FunctionFunctionalInterface {
+    public static void main(String[] args) {
+         //wht pass as parameter and what return
+
+    }
 
 }
