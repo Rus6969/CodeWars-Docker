28a274d986be96ba1856fcca857850c6e1ddfaef
RussellRuslan <SamatovR@aetna.com>
2021-09-01 21:12:46 -0400

    labda generic type

--- src/test/java/FunctionalInterfaces/ConsumerFunctionalInterface.java
+++ src/test/java/FunctionalInterfaces/ConsumerFunctionalInterface.java
@@ -24,23 +24,43 @@ public class ConsumerFunctionalInterface {
         List<String> names = new ArrayList<>();
         names.addAll(Arrays.asList("Ruslan", "Inna", "Vlad"));
         printEach.accept(names);
-//////////////////////////////////////////////////////////////////////////////////////
-        List<String> elements = new ArrayList<>();
-        elements.addAll(Arrays.asList("Java", "Cyber", "Phython", "Java"));
-        List<String> Duplicatedelements = new ArrayList<>();
 
-        Predicate<String>isDuplicated= s -> Collections.frequency(elements,s)>1;
-        elements.forEach(k-> {
-            if(isDuplicated.test(k))
-                Duplicatedelements.add(k);
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        // Consumer<List<Integer>> multiplyBy2 = list -> { }
+
+        numbers.forEach(n -> System.out.println(n * 2));
+
+        System.out.println("----------------------------------------");
+        List<String> elements = new ArrayList<>();
+        elements.addAll(Arrays.asList("Java", "Java", "Cybertek", "Python", "Python"));
+
+        List<String> duplicatedElements = new ArrayList<>();
+
+        Predicate<String> isDuplicated = p -> Collections.frequency(elements, p) > 1;
+
+        elements.forEach(s -> {
+            if (isDuplicated.test(s))
+                if (!duplicatedElements.contains(s))
+                    duplicatedElements.add(s);
         });
 
-        System.out.println(Duplicatedelements);
+        System.out.println(duplicatedElements);
+
+        System.out.println("--------------------------------");
+
+        Consumer<String[]> printDescendingOrder = arr -> {
+            Arrays.sort(arr);
+            for (int i = arr.length - 1; i >= 0; i--) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        };
+
+        //int[] array = {10, 20, 30, 5, 4, 3, 1, 2, 100, 0};
+        String[] array = {"A", "B", "C", "D"};
+        printDescendingOrder.accept(array);
+
+
     }
 
-
-
-    //-----
-
-
-}
+    }
