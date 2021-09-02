6a2e2c3a955c7af9c20085bc7db66557eec57656
RussellRuslan <SamatovR@aetna.com>
2021-09-01 21:01:33 -0400

    labda generic type

--- src/test/java/FunctionalInterfaces/ConsumerFunctionalInterface.java
+++ src/test/java/FunctionalInterfaces/ConsumerFunctionalInterface.java
@@ -2,25 +2,45 @@ package FunctionalInterfaces;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class ConsumerFunctionalInterface {
     public static void main(String[] args) {
-        Consumer<String>printThreetimes = s -> System.out.println(s+s+s);
+        Consumer<String> printThreetimes = s -> System.out.println(s + s + s);
 
         printThreetimes.accept("Ruslan");
         System.out.println("------------------------------------");
 
-        Consumer<List<String>> printEach = list->{
+        Consumer<List<String>> printEach = list -> {
             for (String s : list) {
-                System.out.println(s+" ");
+                System.out.println(s + " ");
             }
             System.out.println();
         };
 
         List<String> names = new ArrayList<>();
-        names.addAll(Arrays.asList("Ruslan","Inna","Vlad"));
+        names.addAll(Arrays.asList("Ruslan", "Inna", "Vlad"));
         printEach.accept(names);
+//////////////////////////////////////////////////////////////////////////////////////
+        List<String> elements = new ArrayList<>();
+        elements.addAll(Arrays.asList("Java", "Cyber", "Phython", "Java"));
+        List<String> Duplicatedelements = new ArrayList<>();
+
+        Predicate<String>isDuplicated= s -> Collections.frequency(elements,s)>1;
+        elements.forEach(k-> {
+            if(isDuplicated.test(k))
+                Duplicatedelements.add(k);
+        });
+
+        System.out.println(Duplicatedelements);
     }
+
+
+
+    //-----
+
+
 }
