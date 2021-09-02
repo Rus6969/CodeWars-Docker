22f7e3d6ddb237f7a68caa07f2ee2a05d45a60e2
RussellRuslan <SamatovR@aetna.com>
2021-09-01 21:40:45 -0400

    Functional

---
+++ src/test/java/FunctionalInterfaces/SupplierInterface.java
@@ -0,0 +1,40 @@
+package FunctionalInterfaces;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.function.Supplier;
+
+//no parameter
+public class SupplierInterface {
+    public static void main(String[] args) {
+
+        Supplier<String> returnCybertek = () -> "Cybertek";
+
+        System.out.println(returnCybertek.get());
+
+        System.out.println("--------------------------------------");
+
+        Supplier<int[]> returnIntArray = () -> {
+            int[] array = new int[5];
+            for (int i = 0; i < array.length; i++) {
+                array[i] = new Random().nextInt();
+            }
+            return array;
+        };
+
+        int[] arr = returnIntArray.get();
+
+        System.out.println(Arrays.toString(arr));
+
+
+        System.out.println("-------------------------");
+
+        Supplier<Double> returnDecimal = () -> {
+            return new Random().nextDouble();
+        };
+
+        System.out.println(returnDecimal.get());
+
+
+    }
+}
