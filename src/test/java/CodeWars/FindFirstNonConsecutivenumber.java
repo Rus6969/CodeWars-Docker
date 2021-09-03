54396adc7f983d5194ecef096318cf9ecb952ffd
RussellRuslan <SamatovR@aetna.com>
2021-09-03 16:44:54 -0400

    non consecuitive integer for loop, instream,Atomic Intweger

--- src/test/java/CodeWars/FindFirstNonConsecutivenumber.java
+++ src/test/java/CodeWars/FindFirstNonConsecutivenumber.java
@@ -1,25 +1,47 @@
 package CodeWars;
 
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 public class FindFirstNonConsecutivenumber {
-    static Integer find(final int[] array) {
-        if(array.length > 0) {
-            for(int i=1; i<array.length; i++) {
-                if(array[i]-1 != array[i-1]){
-                    System.out.println("The element " + array[i] + " at the index " + i + " is the first not consecutive element of the array");
-                    return array[i];
+
+        public static void main(String[] args) {
+            int[] numbers = new int[]{1, 2, 3, 4, 6, 7, 9, 11};
+            findNonConsecutive(numbers);
+            findNonConsecutiveUsingStream(numbers);
+            findNonConsecutiveUsingAtomicInteger(numbers);
+        }
+
+        private static void findNonConsecutive(int[] numbers) {
+            for (int i = 0; i < numbers.length - 1; i++) { // 1, 2, 3, 4, 6, 7, 9, 11
+                int difference = numbers[i + 1] - numbers[i]; // 1
+                if (difference != 1) {
+                    System.out.println("first non consecutive number : " + numbers[i + 1]);
+                    break;
                 }
             }
         }
-        return null; //If we reach the end of the loop, there is no number not consecutive to a previous one.
+
+        private static void findNonConsecutiveUsingStream(int[] numbers) {
+            Integer result = IntStream.range(1, numbers.length-1)
+                    .filter(x -> numbers[x - 1] + 1 != numbers[x]) // 1, 2, 3, 4, 6, 7, 9, 11
+                    .mapToObj(x -> numbers[x])
+                    .findFirst()
+                    .orElse(null);
+
+            System.out.println("first non consecutive number : " + result);
+        }
+
+        private static void findNonConsecutiveUsingAtomicInteger(int[] numbers) {
+            AtomicInteger a = new AtomicInteger(numbers[0]); // 1
+
+            Integer result = Arrays.stream(numbers) // 1, 2, 3, 4, 6, 7, 9, 11
+                    .filter(x -> x != a.getAndIncrement())// 1, 2, 3, 4, 5
+                    .findFirst()
+                    .getAsInt();
+
+            System.out.println("first non consecutive number : " + result);
+        }
+
     }
-
-
-
-    public static void main(String[] args) {
-     int [] arr ={1,2,3,5};
-        System.out.println(find(arr));
-
-    }
-}
--- src/test/java/Instream/StreamPractice.java
+++ src/test/java/Instream/StreamPractice.java
@@ -1,102 +1,51 @@
 package Instream;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class StreamPractice {
 
     public static void main(String[] args) {
 
-//        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-//
-//        list = list.stream().map(e -> e * 3).collect(Collectors.toList());
-//
-//        System.out.println(list);
-//
-//        // add 5 to every single odd numbers in the list
-//        list = list.stream().map(e -> {
-//            if (e % 2 != 0) return e + 5;
-//            return e;
-//        }).collect(Collectors.toList());
-//
-//        System.out.println(list);
-//
-//        System.out.println("---------------------------------------------------");
-//
-//        List<Integer> numbers = list.stream().filter(p -> p % 3 == 0).map(n -> n * 2).collect(Collectors.toList());
-//
-//        System.out.println(numbers);
-//
-//        System.out.println("----------------------------------------------------");
-//
-//        List<String> students = new ArrayList<>();
-//        students.addAll(Arrays.asList("John", "Jimmy", "James", "Breanna", "Shay", "Josh"));
-//
-//        students.stream().skip(3).map(s -> s.toUpperCase()).map(s -> s.charAt(0) + "" + s.charAt(s.length() - 1)).forEach(x -> System.out.println(x));
-//
-//        System.out.println(students);
-//
-//        System.out.println("----------------------------------------------------------------");
-//
-//        Integer[] array = {1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6};
-//        array = Arrays.asList(array).stream().distinct().collect(Collectors.toList()).toArray(new Integer[0]);
-//
-//        System.out.println(Arrays.toString(array));
-//
-//        System.out.println("---------------------------------------------");
-//
-//        List<Integer> nums = new ArrayList<>();
-//        nums.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 10));
-//
-//        nums = nums.stream().limit(7).collect(Collectors.toList());
-//
-//        System.out.println(nums);
-//
-//
-//        System.out.println("------------------------------");
-//
-//        List<Integer> list2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-//        int result = list2.stream().filter(p -> p % 2 != 0).reduce(0, (x, y) -> x + y);
-//        System.out.println(result);
-//
-//        System.out.println("-------------------------------");
-//
-//        List<String> words = new ArrayList<>(Arrays.asList("Java", "Java", "Anna", "Level", "Kayak", "Python", "Bob"));
-//        String palindromes = words.stream().filter(p -> new StringBuilder(p).reverse().toString().equalsIgnoreCase(p))
-//                .reduce("", (x, y) -> x + " " + y);
-//
-//        System.out.println(palindromes);
-//
-//        System.out.println("---------------------------------------------------");
-//
-//        List<Integer> list3 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-//
-//        boolean allEven = list3.stream().allMatch(p -> p % 2 == 0);
-//        boolean allLessThan11 = list3.stream().allMatch(p -> p < 11);
-//        boolean eitherOddOrEven = list3.stream().allMatch(p -> p % 2 == 0 || p % 2 != 0);
-//
-//        System.out.println(allEven);
-//        System.out.println(allLessThan11);
-//        System.out.println(eitherOddOrEven);
-//
-//        System.out.println("------------------------------------------------");
-//
-//        List<Integer> list4 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-//        boolean t1 = list4.stream().anyMatch(p -> p > 5 && p < 9);
-//
-//        System.out.println(t1);
-
-
-        System.out.println("---------------------------------------------");
-
-        List<String> names = new ArrayList<>();
-        names.addAll(Arrays.asList("Java", "Cybertek", "School", "James", "Anna", "Python", "Cava"));
-
-       // String str1 = names.stream().filter(p -> p.toLowerCase().contains("a")).reduce("", (x, y) -> x + " " + y);
-        String str1 = names.stream().filter(p -> p.toLowerCase().contains("a")).reduce("",(x,y)-> y);
-
-        System.out.println(str1);
+        int[] numbers = new int[]{1, 2, 3, 4, 6, 7, 9, 11};
+        findNonConsecutive(numbers);
+        findNonConsecutiveUsingStream(numbers);
+        findNonConsecutiveUsingAtomicInteger(numbers);
     }
+
+    private static void findNonConsecutive(int[] numbers) {
+        for (int i = 0; i < numbers.length - 1; i++) { // 1, 2, 3, 4, 6, 7, 9, 11
+            int difference = numbers[i + 1] - numbers[i]; // 1
+            if (difference != 1) {
+                System.out.println("first non consecutive number : " + numbers[i + 1]);
+                break;
+            }
+        }
+    }
+
+    private static void findNonConsecutiveUsingStream(int[] numbers) {
+        Integer result = IntStream.range(1, numbers.length-1)
+                .filter(x -> numbers[x - 1] + 1 != numbers[x]) // 1, 2, 3, 4, 6, 7, 9, 11
+                .mapToObj(x -> numbers[x])
+                .findFirst()
+                .orElse(null);
+
+        System.out.println("first non consecutive number : " + result);
+    }
+
+    private static void findNonConsecutiveUsingAtomicInteger(int[] numbers) {
+        AtomicInteger a = new AtomicInteger(numbers[0]); // 1
+
+        Integer result = Arrays.stream(numbers) // 1, 2, 3, 4, 6, 7, 9, 11
+                .filter(x -> x != a.getAndIncrement())// 1, 2, 3, 4, 5
+                .findFirst()
+                .getAsInt();
+
+        System.out.println("first non consecutive number : " + result);
+    }
+
 }
