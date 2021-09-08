6026b2c0ae86e5a8b7169b0dd758e92d892c79d2
RussellRuslan <SamatovR@aetna.com>
2021-09-08 15:21:11 -0400

    insertion sort

--- src/test/java/SortingAlgorithms/SelectionSort.java
+++ src/test/java/SortingAlgorithms/SelectionSort.java
@@ -1,4 +1,37 @@
 package SortingAlgorithms;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class SelectionSort {
+
+    public static int[] selectionSort(int[]array){      //[99, 7, 4, -3, 2, 12]
+        for (int i = 0; i < array.length; i++) {
+            int minIndex=i;                     // 0    1  2  3   4  5
+            for (int j = i+1; j < array.length; j++)
+                if (array[j]<array[minIndex]) minIndex=j;                     //  we want sorted keep separate from not sorted
+                swap(array,minIndex,i);
+
+        }
+        return array;
+
+    }
+
+
+
+    private static void swap(int[] arrayx, int index1, int index2) {
+        int temp=arrayx[index1];
+        arrayx[index1]=arrayx[index2];
+        arrayx[index2]=temp;
+    }
+
+    public static void main(String[] args) {
+        var myArray=new int[]{99,7,4,-3,2,12};
+
+        System.out.println(Arrays.toString(myArray));
+        System.out.println("Selection sort ");
+        System.out.println(Arrays.toString(selectionSort(myArray)));
+
+    }
+
 }
