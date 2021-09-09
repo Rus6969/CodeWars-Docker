832120b3e4c1a4e6011c595abf1147068f5da16a
RussellRuslan <SamatovR@aetna.com>
2021-09-09 16:52:28 -0400

    insertion sort

--- src/test/java/SortingAlgorithms/insertionSort.java
+++ src/test/java/SortingAlgorithms/insertionSort.java
@@ -1,4 +1,29 @@
 package SortingAlgorithms;
 
+import java.util.Arrays;
+
 public class insertionSort {
+    public static int[] insertion(int[] array) {
+        // we are starting from second item bc we are doing insertion operation we compare them
+        for (int i = 1; i < array.length; i++) {
+            int current= array[i];
+            int j=i-1; // the last item of a sorted part
+            while(j>=0 && array[j]>current){
+                // shifting operation
+                array[j+1]=array[j];
+                j--; // move to begining of sorted part of array
+            }
+            // j+1 bc we comparing with current if it not grater we stop
+            array[j+1]=current;
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        var myArray=new int[]{99,7,4,-3,2,12};
+
+        System.out.println(Arrays.toString(myArray));
+        System.out.println("insertion_Sort sort ");
+        System.out.println(Arrays.toString(insertion(myArray)));
+    }
 }
