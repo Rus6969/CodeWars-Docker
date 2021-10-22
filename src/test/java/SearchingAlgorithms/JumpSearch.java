f4216cd76abb3f4ffdfc89decb06b9751877389e
RussellRuslan <SamatovR@aetna.com>
2021-10-22 14:12:28 -0400

    Ternary search

---
+++ src/test/java/SearchingAlgorithms/JumpSearch.java
@@ -0,0 +1,24 @@
+package SearchingAlgorithms;
+// improvement to leaner search also worked on sorted arrays
+public class JumpSearch {
+    public static int jumpSearch(int[] array,int data){
+        int blockSize=(int) Math.sqrt(array.length);
+        int start= 0;
+        int next=blockSize;
+        while (start<array.length && array[next-1]<data){
+            start=next;
+            next+=blockSize;
+            if (next>array.length) next=array.length;
+        }
+        for (int i = start; i < next; i++) { if (array[i]==data) return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6,7,8,9};
+        System.out.println(jumpSearch(arr,4));
+    }
+
+
+}
