68c8e0bd822c5abc84d88c013164e76cf599ef34
Rus6969 <russam4515@gmail.com>
2022-03-26 15:40:35 -0400

    allComboWithFile

---
+++ src/test/java/interviewQuestions/Caribou/howTasksLookOpenAsSeparateProject/dsfdsf.java
@@ -0,0 +1,7 @@
+package interviewQuestions.Caribou.howTasksLookOpenAsSeparateProject;
+
+public class dsfdsf {
+    public static void main(String[] args) {
+        System.out.println("dlsfnjpsd");
+    }
+}
