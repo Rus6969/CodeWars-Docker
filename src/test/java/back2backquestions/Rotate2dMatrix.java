b8947e0039c57397960e9342a0dfa88f07f54aca
Rus6969 <russam4515@gmail.com>
2022-10-23 19:50:21 -0400

    Rotate2dMatrix

---
+++ src/test/java/back2backquestions/Rotate2dMatrix.java
@@ -0,0 +1,24 @@
+package back2backquestions;
+/*
+Given a two-dimensional square matrix (n x n),
+rotate the matrix 90 degrees to the right (clockwise).
+Input:
+[
+  [10, 20],
+  [30, 40]
+],
+
+Output:
+[
+ [30, 10],
+ [40, 20]
+]
+
+ */
+
+public class Rotate2dMatrix {
+    public int[][] rotate(int[][] matrix) {
+
+
+    }
+}
