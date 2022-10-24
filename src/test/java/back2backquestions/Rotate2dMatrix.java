0e536ae9664e5b4358b818a85bdc46e74bea6c1b
Rus6969 <russam4515@gmail.com>
2022-10-23 21:51:39 -0400

    Rotate2dMatrix

--- src/test/java/back2backquestions/Rotate2dMatrix.java
+++ src/test/java/back2backquestions/Rotate2dMatrix.java
@@ -18,6 +18,26 @@ Output:
 
 public class Rotate2dMatrix {
     public int[][] rotate(int[][] matrix) {
+        int N = matrix.length;
+        // step number 1 we need convert rows into columns
+        for (int i = 0; i < N; i++) {
+            for (int j = i; j < N; j++) {
+                int temp = matrix[i][j] = matrix[j][i];
+                matrix[i][j] = matrix[j][i];
+                // this step  we do not want swipe twice
+                matrix[j][i] = temp;
+            }
+        }
+        // step 2 swap inside each row
+
+        for (int i = 0; i < N; i++) {
+            for (int j = i; j < (N/2); j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][N-1-j];
+                // this step  we do not want swipe twice
+                matrix[i][N-1-j] = temp;
+            }
+        }
 
 
     }
