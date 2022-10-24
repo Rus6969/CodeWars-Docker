05d5028583766018dd9d81890d1a7b5e07f56546
Rus6969 <russam4515@gmail.com>
2022-10-23 22:07:55 -0400

    Rotate2dMatrixchange

--- src/test/java/back2backquestions/Rotate2dMatrix.java
+++ src/test/java/back2backquestions/Rotate2dMatrix.java
@@ -31,7 +31,7 @@ public class Rotate2dMatrix {
         // step 2 swap inside each row
 
         for (int i = 0; i < N; i++) {
-            for (int j = i; j < (N/2); j++) {
+            for (int j = 0; j < (N/2); j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[i][N-1-j];
                 // this step  we do not want swipe twice
